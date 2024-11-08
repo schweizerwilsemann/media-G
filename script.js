da688615190f98072b028b268f1d815a4004377e
project_Html <khoanguyenduc99@gmail.com>
2024-11-08 09:55:40 +0700

    update

--- script.js
+++ script.js
@@ -4,20 +4,60 @@ const images = [
   "images/person.png",
   "images/person.png",
 ];
-
+const texts = [
+  "Wish I'd gotten solar panels sooner. My wife won't stop talking about how much we're saving. We’re saving $300 a month on electric bills.",
+  "Wish I'd gotten solar panels sooner. My wife won't stop talking about how much we're saving. We’re saving $300 a month on electric bills.",
+  "Wish I'd gotten solar panels sooner. My wife won't stop talking about how much we're saving. We’re saving $300 a month on electric bills.",
+  "Wish I'd gotten solar panels sooner. My wife won't stop talking about how much we're saving. We’re saving $300 a month on electric bills.",
+];
 let currentIndex = 0;
 
 const showContent = (index) => {
   const element = document.getElementById("slider-content");
+  const textElement = document.getElementById("slider-text");
   element.style.transform = "translateX(-100%)";
+  textElement.style.transform = "translateX(-100%)";
 
   setTimeout(() => {
     element.src = images[index];
+    textElement.textContent = texts[index];
+
     element.style.transition = "none";
+    textElement.style.transition = "none";
+
+    textElement.style.transform = "translateX(100%)";
     element.style.transform = "translateX(100%)";
     setTimeout(() => {
       element.style.transition = "transform 0.5s ease-in-out";
+      textElement.style.transition = "transform 0.5s ease-in-out";
+
       element.style.transform = "translateX(0)";
+      textElement.style.transform = "translateX(0)";
+    }, 50);
+  }, 500);
+};
+
+const showPrev = (index) => {
+  const element = document.getElementById("slider-content");
+  const textElement = document.getElementById("slider-text");
+  element.style.transform = "translateX(100%)";
+  textElement.style.transform = "translateX(100%)";
+
+  setTimeout(() => {
+    element.src = images[index];
+    textElement.textContent = texts[index];
+
+    element.style.transition = "none";
+    textElement.style.transition = "none";
+
+    textElement.style.transform = "translateX(-100%)";
+    element.style.transform = "translateX(-100%)";
+    setTimeout(() => {
+      element.style.transition = "transform 0.5s ease-in-out";
+      textElement.style.transition = "transform 0.5s ease-in-out";
+
+      element.style.transform = "translateX(0)";
+      textElement.style.transform = "translateX(0)";
     }, 50);
   }, 500);
 };
@@ -29,7 +69,7 @@ const next = () => {
 
 const prev = () => {
   currentIndex = (currentIndex - 1 + images.length) % images.length;
-  showContent(currentIndex);
+  showPrev(currentIndex);
 };
 
 window.onload = () => {
